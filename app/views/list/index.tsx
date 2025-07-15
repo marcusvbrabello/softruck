ce04b1f818299ebc1b5663c4eb02f9e9f6dfe3ae
Marcus Rabello <marcusvbrabello@gmail.com>
2025-07-15 10:18:38 -0300

    refactor List component to integrate Loading and enhance styling

--- app/views/list/index.tsx
+++ app/views/list/index.tsx
@@ -1,19 +1,89 @@
-import StatusBar from "@components/StatusBar";
+import Loading from "@components/Loading";
 import { GenericText } from "@components/Texts/GenericText";
-import { View } from "react-native";
+import useListViewModel from "app/view_models/list";
+import { useEffect } from "react";
+import { FlatList, Image, TouchableOpacity, View } from "react-native";
+import { style, vehicleColor } from "./style";
 
 export default function List() {
+	const {
+		container,
+		content,
+		wrapLoading,
+		vehicleImage,
+		trackButton,
+		contentContainerStyle,
+		topContent,
+		infoTopContent,
+	} = style();
+
+	const {
+		vehicle,
+		tracks,
+		isLoadingList,
+		handleListData,
+		formatDate,
+		formatDistance,
+		goTrack,
+	} = useListViewModel();
+
+	useEffect(() => {
+		handleListData();
+	}, []);
+
 	return (
 		<>
-			<StatusBar />
-			<View
-				style={{
-					flex: 1,
-					justifyContent: "center",
-					alignItems: "center",
-				}}
-			>
-				<GenericText text="List" size="LARGE" lineHeight="LARGE" />
+			<View style={container}>
+				{isLoadingList ? (
+					<View style={wrapLoading}>
+						<Loading />
+					</View>
+				) : (
+					<View style={content}>
+						<View style={topContent}>
+							<Image
+								source={{ uri: vehicle?.picture?.address }}
+								style={vehicleImage}
+							/>
+
+							<View style={infoTopContent}>
+								<View style={vehicleColor(vehicle?.color)} />
+								<GenericText
+									text={vehicle?.plate}
+									size="LARGE"
+									lineHeight="LARGE"
+									font="SEMI_BOLD"
+								/>
+							</View>
+						</View>
+
+						<FlatList
+							data={tracks}
+							keyExtractor={(_, index) => index.toString()}
+							renderItem={({ item }) => (
+								<TouchableOpacity
+									onPress={() => goTrack(item)}
+									style={trackButton}
+								>
+									<GenericText
+										text={formatDate(item?.start_at)}
+										size="MEDIUM"
+										lineHeight="MEDIUM"
+										font="REGULAR"
+									/>
+									<GenericText
+										text={formatDistance(item?.distance)}
+										size="MEDIUM"
+										lineHeight="MEDIUM"
+										font="REGULAR"
+									/>
+								</TouchableOpacity>
+							)}
+							showsVerticalScrollIndicator={false}
+							contentContainerStyle={contentContainerStyle}
+						/>
+					</View>
+				)}
 			</View>
 		</>
 	);
--- app/views/list/style.ts
+++ app/views/list/style.ts
@@ -1,1 +1,54 @@
-export const style = () => {};
+import resizePixel from "@utils/resizePixel";
+import { StyleSheet, ViewStyle } from "react-native";
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingVertical: resizePixel(32, "height"),
+		paddingHorizontal: resizePixel(32, "width"),
+	},
+	content: {
+		flex: 1,
+		alignItems: "center",
+	},
+	wrapLoading: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	vehicleImage: {
+		borderRadius: resizePixel(50),
+		width: resizePixel(100, "width"),
+		height: resizePixel(100, "height"),
+	},
+	trackButton: {
+		paddingVertical: resizePixel(12, "height"),
+		paddingHorizontal: resizePixel(12, "width"),
+		marginVertical: resizePixel(6, "height"),
+		backgroundColor: "#f5f5f5",
+		borderRadius: resizePixel(8),
+		borderWidth: 1,
+		minWidth: "100%",
+	},
+	contentContainerStyle: {
+		paddingTop: resizePixel(16, "height"),
+	},
+	topContent: {
+		alignItems: "center",
+		gap: resizePixel(8, "height"),
+	},
+	infoTopContent: {
+		flexDirection: "row",
+		alignItems: "center",
+		gap: resizePixel(8, "width"),
+	},
+});
+
+export const vehicleColor = (color: string): ViewStyle => ({
+	width: resizePixel(16, "width"),
+	height: resizePixel(16, "height"),
+	borderRadius: resizePixel(8),
+	backgroundColor: color,
+});
+
+export const style = () => styles;
