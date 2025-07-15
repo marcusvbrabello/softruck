4a24b3a1befb5d593fe3a7244feb8e87109f75b7
Marcus Rabello <marcusvbrabello@gmail.com>
2025-07-15 11:00:40 -0300

    add i18n support and localization dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import { useTheme } from "@utils/useThemeColor";
 import { Stack } from "expo-router";
+import i18n from "./i18n";
 
 export default function RootLayout() {
 	const colors = useTheme();
@@ -19,11 +20,11 @@ export default function RootLayout() {
 		>
 			<Stack.Screen
 				name="views/list/index"
-				options={{ title: "Lista de Viagens" }}
+				options={{ title: i18n.t("select_route") }}
 			/>
 			<Stack.Screen
 				name="views/track/index"
-				options={{ title: "Detalhes da Viagem" }}
+				options={{ title: i18n.t("route_details") }}
 			/>
 		</Stack>
 	);
