ed2c7592b4ed29a451de4a8bcbf2a3e83d95f7a7
Marcus Rabello <marcusvbrabello@gmail.com>
2025-07-16 22:40:01 -0300

    feat: create Storage type and initialize storage constants

---
+++ app/@types/constants/storage.ts
@@ -0,0 +1,4 @@
+export type Storage = {
+	language: string;
+	theme: string;
+};
---
+++ app/constants/storage.ts
@@ -0,0 +1,6 @@
+import { Storage } from "app/@types/constants/storage";
+
+export const storage = {
+	language: "@softruck/language",
+	theme: "@softruck/theme",
+} as Storage;
